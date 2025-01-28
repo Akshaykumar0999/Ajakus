90f05f6b676608eec5bf2184363b1825f8289e51
Akshay <kanikeakshay55@gmail.com>
2025-01-28 10:32:57 +0530

    ajakus theme styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { useContext } from 'react';
 import './App.css'
 import DashBoard from './components/Users/users';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { ThemeContext } from './context/ThemeContext';
 function App() {
+  const data = useContext(ThemeContext)
   return (
-    <div className='paint-inventory-main-container'>
+    <div className='paint-inventory-main-container' style={{
+      background: data.dark ? '#212631' : '#fff',
+      color: data.dark ? '#fff' : '#212631'
+  }}>
       <DashBoard />
     </div>
   )
